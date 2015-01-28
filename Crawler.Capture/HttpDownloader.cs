0886feb0e14f9e6d7ddc87a7bd4044d3a9bf222a
xsi640 <xsi64@126.com>
2015-01-29 00:14:03 +0800

    基础类

---
+++ Crawler.Capture/HttpDownloader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Crawler.Capture
+{
+    public class HttpDownloader
+    {
+        public string GetConetn(string url)
+        {
+            string result = string.Empty;
+            var webRequest = WebRequest.Create(url);
+            using (var response = webRequest.GetResponse())
+            {
+                using (var content = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(content))
+                    {
+                        result = reader.ReadToEnd();
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
