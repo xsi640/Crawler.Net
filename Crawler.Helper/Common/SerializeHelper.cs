f01a1f893c083c1053095d902a95ce3a7a0e9e4d
xsi640 <xsi64@126.com>
2015-01-28 23:19:39 +0800

    导入Helper.Common

---
+++ Crawler.Helper/Common/SerializeHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crawler.Helper.Common
+{
+    class SerializeHelper
+    {
+    }
+}
---
+++ Crawler.Helper/Common/UtilHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crawler.Helper.Common
+{
+    public static class UtilHelper
+    {
+
+        public static int GetRamdom(int minValue, int maxValue)
+        {
+            return StaticRamdom.Next(minValue, maxValue);
+        }
+
+        public static long GetRamdom(long minValue, long maxValue)
+        {
+            return StaticRamdom.Next(minValue, maxValue);
+        }
+    }
+}
